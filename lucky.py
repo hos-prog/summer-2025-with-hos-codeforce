fe922a7d37813764609b99742e6c4de70821e568
hos-prog <hosamaadem8@gmail.com>
2025-06-14 12:09:49 +0300

    Create lucky.py
    june 14 2025

---
+++ lucky.py
@@ -0,0 +1,8 @@
+#HosamaAdem
+t=int(input())
+for i in range(t):
+    k=input()
+    if int(k[0])+int(k[1])+int(k[2])==int(k[3])+int(k[4])+int(k[5]):
+        print("YES")
+    else:
+        print("NO")
