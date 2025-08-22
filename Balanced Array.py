b5c29b5b9f21f47b8768a2fba3800e131b490f0e
hos <hosamaadem8@gmail.com>
2025-08-22 23:15:11 +0300

    Create Balanced Array.py

---
+++ Balanced Array.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    
+    if (n // 2) % 2 != 0:
+        print("NO")
+    else:
+        print("YES")
+        even = [i*2 for i in range(1, n//2 + 1)]
+        odd = [i*2-1 for i in range(1, n//2)]
+        odd.append(sum(even) - sum(odd))
+        print(*(even + odd))
+
